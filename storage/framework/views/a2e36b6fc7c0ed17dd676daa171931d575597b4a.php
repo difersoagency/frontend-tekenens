1dea6a6d72c8b3e40080d868778e7d4e4b9dbb2e
roynald <roynald@gradin.co.id>
2022-06-05 18:08:18 +0700

    initial commit

---
+++ storage/framework/views/a2e36b6fc7c0ed17dd676daa171931d575597b4a.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('assets/css/bootstrap.css')); ?>">
+    <title>Document</title>
+</head>
+<body class="m-0 overflow-hidden">
+    <img src="../../assets/images/loadingBanner.png" alt="Loading Banner" class="w-100 h-100" width="100%" height="100%">
+</body>
+</html><?php /**PATH D:\Project\tekenens_project\resources\views/pages/loading.blade.php ENDPATH**/ ?>
